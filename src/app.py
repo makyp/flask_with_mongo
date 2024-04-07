c0e2595a8f932c45474e33d13fc7fcd3fd97105a
Maira Quiroga <maquip02@hotmail.com>
2024-04-06 22:29:51 -0500

    Se agrego una barra de navegación para acceder a cada una de las opciones deseadas de una mejor manera

--- src/app.py
+++ src/app.py
@@ -28,26 +28,47 @@ def agregarPersona():
         return redirect(url_for('index'))
     else: 
         return "Error"
-##Ruta para eliminar un dato   
-@app.route('/eliminar_persona/<string:nombre_persona>')
-def eliminar(nombre_persona):
-    colection_personas = con_bd['Personas']
-    colection_personas.delete_one({'nombre': nombre_persona})
-    return redirect(url_for('index'))
+# ##Ruta para eliminar un dato   
+# @app.route('/eliminar_persona/<string:nombre_persona>')
+# def eliminar(nombre_persona):
+#     colection_personas = con_bd['Personas']
+#     colection_personas.delete_one({'nombre': nombre_persona})
+#     return redirect(url_for('index'))
+@app.route('/eliminar_persona', methods=['GET', 'POST'])
+def eliminar_persona():
+    if request.method == 'POST':
+        nombre_persona = request.form['nombre_persona']
+        coleccion_personas = con_bd['Personas']
+        persona = coleccion_personas.find_one({'nombre': nombre_persona})
+        if persona:
+            coleccion_personas.delete_one({'nombre': nombre_persona})
+            return redirect(url_for('index'))
+        else:
+            return "La persona especificada no existe."
+    else:
+        coleccion_personas = con_bd['Personas']
+        personas = coleccion_personas.find() 
+        return render_template('eliminar.html', personas=personas)
 
-@app.route("/editar_personas/<string:nombre_persona>", methods=['GET','POST'])
+
+@app.route("/editar_personas/<string:nombre_persona>", methods=['GET', 'POST'])
 def editar(nombre_persona):
     colection_personas = con_bd['Personas']
-    ##Obtengo los datos del index para mostrarlos en el formulario de actualización
-    nombre = request.form['nombre']
-    apellido = request.form['apellido']
-    telefono = request.form['telefono']
-    if nombre and apellido and telefono:
-        ##el update_one solicita un identificador en este caso el nombre de las personas y los datos a modificar
-        colection_personas.update_one({'nombre':nombre_persona}, {'$set':{'nombre':nombre,'apellido':apellido, 'telefono': telefono}})
-        return redirect(url_for('index'))
+    personas = colection_personas.find()  # Obtener todas las personas
+    
+    if request.method == 'POST':
+        nombre = request.form.get('nombre')
+        apellido = request.form.get('apellido')
+        telefono = request.form.get('telefono')
+        
+        if nombre and apellido and telefono:
+            colection_personas.update_one({'nombre': nombre_persona}, {'$set': {'nombre': nombre, 'apellido': apellido, 'telefono': telefono}})
+            return redirect(url_for('index'))
+        else:
+            return "Error: Todos los campos son obligatorios"
     else:
-        return "Error"
+        return render_template('editar.html', personas=personas)  # Pasar todas las personas a la plantilla
+
 
 
   
